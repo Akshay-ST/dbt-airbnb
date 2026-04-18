739f9c9b5dfcf762ab2d7a2639e52c986eb5d233
akshay <akshayst@aol.com>
2026-04-19 00:04:51 +0530

    Files Added

--- airbnb/models/dim/dim_hosts_cleansed.sql
+++ airbnb/models/dim/dim_hosts_cleansed.sql
@@ -9,6 +9,7 @@ WITH src_hosts AS (
 SELECT
     host_id,
     NVL(host_name,'Anonymous') as host_name,
+    -- IFF(is_superhost = 't', TRUE, FALSE) AS is_superhost,
     is_superhost,
     created_at,
     updated_at
